49c7b37098d3ef371fae571fb9438b24e33e1fbf
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-26 12:50:44 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,20 @@
+package com.JavaBean;
+
+public class Demo 
+{
+private String name;
+private int age;
+Demo(String name,int age)
+{
+	this.name=name;
+	this.age=age;
+}
+public String getname()
+{
+ return name;
+}
+public int getage()
+{
+ return age;
+}
+}
